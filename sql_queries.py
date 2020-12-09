e104c18b7f5e1b7e2e2f63f27432746c49b83713
NoceanAI <noceanai@gmail.com>
2020-12-09 11:11:04 +0100

    adding image

--- sql_queries.py
+++ sql_queries.py
@@ -8,41 +8,110 @@ time_table_drop = "drop table if exists time"
 
 # CREATE TABLES
 
-songplay_table_create = (""" CREATE TABLE songplays (songplay_id INT PRIMARY KEY, start_time TIMESTAMP NOT NULL, user_id int NOT NULL, level varchar, song_id varchar, artist_id varchar, session_id INT, location varchar, user_agent varchar) """)
+songplay_table_create = (""" CREATE TABLE songplays (
+                                                    songplay_id INT PRIMARY KEY, 
+                                                    start_time TIMESTAMP NOT NULL, 
+                                                    user_id int NOT NULL, 
+                                                    level varchar, 
+                                                    song_id varchar, 
+                                                    artist_id varchar, 
+                                                    session_id INT, 
+                                                    location varchar, 
+                                                    user_agent varchar) 
+                                                    """)
 
-user_table_create = (""" CREATE TABLE users (user_id INT PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar) """)
+user_table_create = (""" CREATE TABLE users (
+                                            user_id INT PRIMARY KEY, 
+                                            first_name varchar, 
+                                            last_name varchar,
+                                            gender varchar, 
+                                            level varchar) 
+                                            """)
 
-song_table_create = (""" CREATE TABLE songs (song_id varchar PRIMARY KEY NOT NULL, title varchar, artist_id varchar, year int, duration NUMERIC) """)
+song_table_create = (""" CREATE TABLE songs (
+                                            song_id varchar PRIMARY KEY NOT NULL, 
+                                            title varchar, 
+                                            artist_id varchar, 
+                                            year int, 
+                                            duration NUMERIC) 
+                                            """)
 
-artist_table_create = (""" CREATE TABLE artists (artist_id VARCHAR PRIMARY KEY, artist_name varchar, artist_location varchar, artist_latitude varchar, artist_longitude varchar) """)
+artist_table_create = (""" CREATE TABLE artists (
+                                                artist_id VARCHAR PRIMARY KEY, 
+                                                artist_name varchar, 
+                                                artist_location varchar, 
+                                                artist_latitude varchar, 
+                                                artist_longitude varchar) 
+                                                """)
 
-time_table_create = (""" CREATE TABLE time (start_time TIMESTAMP, hour NUMERIC, day NUMERIC, week NUMERIC, month NUMERIC, year NUMERIC, weekday NUMERIC)""")
+time_table_create = (""" CREATE TABLE time (
+                                            start_time TIMESTAMP PRIMARY KEY, 
+                                            hour NUMERIC, 
+                                            day NUMERIC, 
+                                            week NUMERIC, 
+                                            month NUMERIC,
+                                            year NUMERIC, 
+                                            weekday NUMERIC)
+                                            """)
 
 # INSERT RECORDS
 
-songplay_table_insert = (""" INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+songplay_table_insert = (""" INSERT INTO songplays (
+                                                    songplay_id, 
+                                                    start_time, 
+                                                    user_id, 
+                                                    level, 
+                                                    song_id, 
+                                                    artist_id, 
+                                                    session_id, 
+                                                    location, 
+                                                    user_agent) \
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                         ON CONFLICT (songplay_id) DO NOTHING
 """)
 
-user_table_insert = ("""INSERT INTO users ("user_id", "first_name", "last_name", "gender", "level") \
+user_table_insert = ("""INSERT INTO users (
+                                                "user_id", 
+                                                "first_name", 
+                                                "last_name", 
+                                                "gender", 
+                                                "level") \
                     VALUES (%s, %s, %s, %s, %s)
-                    ON CONFLICT (user_id) DO NOTHING
+                    ON CONFLICT (user_id) DO UPDATE
+                    SET level = users.level;
 """)
 
-song_table_insert = ("""INSERT INTO songs ("song_id", "title", "artist_id", "year", "duration") \
+song_table_insert = ("""INSERT INTO songs (
+                                            "song_id", 
+                                            "title", 
+                                            "artist_id", 
+                                            "year", 
+                                            "duration") \
                     VALUES (%s, %s, %s, %s, %s)
                     ON CONFLICT (song_id) DO NOTHING
 """)
 
-artist_table_insert = (""" INSERT INTO artists ("artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude") \
+artist_table_insert = (""" INSERT INTO artists (
+                                                "artist_id", 
+                                                "artist_name", 
+                                                "artist_location", 
+                                                "artist_latitude", 
+                                                "artist_longitude") \
                     VALUES (%s, %s, %s, %s, %s)
                     ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
-time_table_insert = (""" INSERT INTO time ("start_time", "year", "month", "week", "weekday", "day", "hour") \
+time_table_insert = (""" INSERT INTO time (
+                                            "start_time", 
+                                            "year", 
+                                            "month", 
+                                            "week", 
+                                            "weekday", 
+                                            "day",
+                                            "hour") \
                     VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
